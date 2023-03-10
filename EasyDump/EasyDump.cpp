445faa1e553844c8f7bd1185ca4cc9c3dfdc7d05
mllimo <ar.guijarro.contreras@gmail.com>
2023-03-10 16:29:43 +0000

    init

---
+++ EasyDump/EasyDump.cpp
@@ -0,0 +1,12 @@
+﻿// EasyDump.cpp: define el punto de entrada de la aplicación.
+//
+
+#include "EasyDump.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
